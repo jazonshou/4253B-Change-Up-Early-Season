1874f8a1a7a56fd737e6e55936d2aecc50afcd15
Jason Zhou <24JasonZ@tas.edu.tw>
2020-10-31 17:20:15 +0800

    forgot to push these lol

--- include/subsystems/intakes.hpp
+++ include/subsystems/intakes.hpp
@@ -5,5 +5,11 @@ void setIntakes(int left, int right);
 
 void setIndexer(int bottom, int top);
 
+void resetLineSensors();
+
 //DRIVER CONTROL FUNCTIONS
 void autoIndex();
+
+void cycle();
+
+void intakeOne();
--- src/subsystems/globals.cpp
+++ src/subsystems/globals.cpp
@@ -1,28 +1,28 @@
 #include "main.h"
 
 //DRIVE MOTORS
-Motor leftFront(3, MOTOR_GEARSET_18, false, MOTOR_ENCODER_COUNTS);
-Motor rightFront(9, MOTOR_GEARSET_18, true, MOTOR_ENCODER_COUNTS);
-Motor leftBack(2, MOTOR_GEARSET_18, false, MOTOR_ENCODER_COUNTS);
-Motor rightBack(10, MOTOR_GEARSET_18, true, MOTOR_ENCODER_COUNTS);
+Motor leftFront(9, MOTOR_GEARSET_18, true, MOTOR_ENCODER_COUNTS);
+Motor rightFront(3, MOTOR_GEARSET_18, false, MOTOR_ENCODER_COUNTS);
+Motor leftBack(8, MOTOR_GEARSET_18, true, MOTOR_ENCODER_COUNTS);
+Motor rightBack(2, MOTOR_GEARSET_18, false, MOTOR_ENCODER_COUNTS);
 
-//CONTROLLER
+//CONTROLLER OP
 Controller master(E_CONTROLLER_MASTER);
 
 //MISCELLANEUOUS (MOTORS)
-Motor intakeLeft(18, MOTOR_GEARSET_06, false, MOTOR_ENCODER_COUNTS);
-Motor intakeRight(20, MOTOR_GEARSET_06, true, MOTOR_ENCODER_COUNTS);
+Motor intakeLeft(18, MOTOR_GEARSET_06, true, MOTOR_ENCODER_COUNTS);
+Motor intakeRight(20, MOTOR_GEARSET_06, false, MOTOR_ENCODER_COUNTS);
 Motor indexerBottom(17, MOTOR_GEARSET_06, false, MOTOR_ENCODER_COUNTS);
 Motor indexerTop(19, MOTOR_GEARSET_06, false, MOTOR_ENCODER_COUNTS);
 
 //DRIVE SENSORS
-ADIEncoder encoderLeft('C', 'D', true);
-ADIEncoder encoderRight('G', 'H', false);
-ADIEncoder encoderBack('A', 'B');
+ADIEncoder encoderLeft('C', 'D', false);
+//ADIEncoder encoderRight('G', 'H', false);
 Imu inertialBoi(1);
 
 //MISCELLANEUOUS (SENSORS)
-ADILineSensor lineSensorBottom('C');
-ADILineSensor lineSensorMiddle('D');
+ADILineSensor lineSensorBottom('F');
+ADILineSensor lineSensorMiddle('B');
 ADILineSensor lineSensorTop('G');
+ADIButton topLimitSwitch('A');
 ADIButton limitSwitch('E');
